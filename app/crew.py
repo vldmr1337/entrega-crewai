1b4ac73a71d84cc1845ee03a8a5ed7a2ec3ef6e4
vldmr1337 <netok812@gmail.com>
2025-04-09 19:25:15 -0300

    Ajustes nas tasks dos agentes, para uma pesquisa e redação mais precisa.

--- app/crew.py
+++ app/crew.py
@@ -6,14 +6,14 @@ def create_crew(query: str) -> Crew:
     # Tarefa do Pesquisador
     task_research = Task(
         description=f"Pesquisar sobre o tema: {query}. Use fontes como Wikipédia.",
-        expected_output="Resumo detalhado com links úteis",
+        expected_output="Resumo detalhado com links úteis(referências), no minimo 500 palavras",
         agent=researcher
     )
 
     # Tarefa do Redator
     task_write = Task(
-        description=f"Escrever artigo sobre '{query}' com pelo menos 300 palavras, baseado na pesquisa anterior.",
-        expected_output="Artigo completo com título, conteúdo(apenas os paragrafos) e referências.",
+        description=f"Escrever um artigo sobre '{query}' com no MINIMO 300 palavras, baseado na pesquisa anterior.",
+        expected_output="Artigo completo com título, conteúdo(apenas os paragrafos, e no mínimo 300 palavras) e referências.",
         agent=writer,
         output_pydantic=Article,
     )
